6b2a963ff767f34efbdc33f54fc6bba2992cf608
Ark-Arch <davidagbemuko.ad@gmail.com>
2025-07-19 15:20:52 +0100

    successful test of the home route for the auth-service

--- auth-service/app/api/v1/routes.py
+++ auth-service/app/api/v1/routes.py
@@ -5,3 +5,8 @@ router = APIRouter() # helps create a new router instance
 @router.get('/')
 def root():
     return {"message": "The Auth service is up and running now"}
+
+
+@router.get('/test')
+def root():
+    return {"message": "this is a test route"}
